597e3eac3150c57db7043062e0c2cda98d7e039c
Gabriel-sardinha <gabrielsardinhaferreira@gmail.com>
2022-02-15 22:10:37 -0300

    adicionando errorpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,32 @@
+import { CursosTaguatingaComponent } from './cursos/cursos-taguatinga/cursos-taguatinga.component';
+import { CursosAsaNorteComponent } from './cursos/cursos-asa-norte/cursos-asa-norte.component';
+import { MonitoresTaguatingaComponent } from './monitores/monitores-taguatinga/monitores-taguatinga.component';
+import { MonitoresAsaNorteComponent } from './monitores/monitores-asa-norte/monitores-asa-norte.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
   {
     path: 'monitores',
-    loadChildren: () => import('./monitores/monitores.module').then(m => m.MonitoresModule)
+    loadChildren: () =>
+      import('./monitores/monitores.module').then((m) => m.MonitoresModule),
   },
   {
     path: 'cursos',
-    loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule)
-  }
+    loadChildren: () =>
+      import('./cursos/cursos.module').then((m) => m.CursosModule),
+  },
+  { path: '**', component: ErrorPageComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
