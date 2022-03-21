f291f2d74772d717bcebfedc9d031c1ca4a51d55
Gabriel <gabrielsardinhaferreira@gmail.com>
2022-03-21 16:36:22 -0300

    arrumando curso taguatinga

--- src/app/cursos/cursos-taguatinga/cursos-taguatinga.component.ts
+++ src/app/cursos/cursos-taguatinga/cursos-taguatinga.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CursosTaguatingaComponent implements OnInit {
 
   cursos: any[] = [
-    {nome: 'Linguagem Java Básico', periodo: '28/03/22 a 05/04/22', estudo: '06/04/22', avaliacao: '07/04/22', horario: 'das 11h às 12h30', inscricao:'https://forms.gle/LWVfwRECPwQnzRr98'},
+    {nome: 'Linguagem Java Básico', periodo: '28/03/22 a 05/04/22', estudo: '06/04/22', avaliacao: '07/04/22', horario: 'das 11h às 12h30', inscricao:'https://forms.gle/e1s4CpqXUBWRXdTN8'},
     {nome: 'Linux', periodo: '25/04/22 a 03/05/22', estudo: '04/05/22', avaliacao: '05/05/22', horario: 'das 11h às 12h30', inscricao:''},
     {nome: 'HTML - CSS', periodo: '30/05/22 a 07/06/22', estudo: '08/06/22', avaliacao: '09/06/22', horario: 'das 11h às 12h30', inscricao:''}
   ]
