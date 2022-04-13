20e52cc63dc0e9c44c4eb2e3ae17ae71e42b6c86
Gabriel <gabrielsardinhaferreira@gmail.com>
2022-04-12 23:15:40 -0300

    adicionando novos monitores e alterando monitores asa norte

--- src/app/monitores/monitores-asa-norte/monitores-asa-norte.component.ts
+++ src/app/monitores/monitores-asa-norte/monitores-asa-norte.component.ts
@@ -9,8 +9,10 @@ export class MonitoresAsaNorteComponent implements OnInit {
 
 
   monitores: any[] = [
-    {nome: "Érica Moraes Abdão", email: "erica.abdao@sempreceub.com", horario: "17h às 19h10"},
-    {nome: "Lucas Borges Vieira", email: "lucasborges@sempreceub.com", horario: "17h às 19h10"},
+    {nome: "Érica Moraes Abdão", email: "erica.abdao@sempreceub.com", horario: "17h30 às 19h"},
+    {nome: "Lucas Borges Vieira", email: "lucasborges@sempreceub.com", horario: "17h30 às 19h"},
+    {nome: "Victor Aragão Mayer Duarte", email: "victor.aragao@sempreceub.com", horario: "17h30 às 19h"},
+    {nome: "Carlos David Castro de Souza Neto", email: "dauid64@sempreceub.com", horario: "17h30 às 19h"},
   ]
 
   constructor() { }
--- src/app/monitores/monitores-taguatinga/monitores-taguatinga.component.ts
+++ src/app/monitores/monitores-taguatinga/monitores-taguatinga.component.ts
@@ -11,6 +11,11 @@ export class MonitoresTaguatingaComponent implements OnInit {
   monitores: any[] = [
     {nome: "Ítalo Vinicius Neves Cordeiro", email: "lobosolitario@sempreceub.com", horario: "11h às 12h30"},
     {nome: "Antônio Vitor Arruda Vilela", email: "antonio.vitor@sempreceub.com", horario: "11h às 12h30"},
+    {nome: "André Gustavo de Oliveira dos Santos", email: "andreugabagos@sempreceub.com", horario: "11h às 12h30"},
+    {nome: "Davi Siqueira Corrêa Galati", email: "davigalati@gmail.com", horario: "11h às 12h30"},
+    {nome: "Enzo Alves Miranda Leite", email: "enzo0402@sempreceub.com", horario: "11h às 12h30"},
+    {nome: "Jeferson Marques Rodrigues", email: "joedf1006@sempreceub.com", horario: "11h às 12h30"},
+
   ]
 
   constructor() { }
