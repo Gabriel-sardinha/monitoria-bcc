b26a272e0d624c67938321d8daa59e48f6d6fa5d
Gabriel-sardinha <gabrielsardinhaferreira@gmail.com>
2022-04-20 00:06:56 -0300

    subindo direto

--- src/app/cursos/cursos-asa-norte/cursos-asa-norte.component.ts
+++ src/app/cursos/cursos-asa-norte/cursos-asa-norte.component.ts
@@ -13,7 +13,7 @@ export class CursosAsaNorteComponent implements OnInit {
     {nome: 'Linguagem Python Básico', periodo: '30/05/22 a 10/06/22', avaliacao: '13/06/22', horario: 'das 17h30 às 19h', inscricao:''},
   ]
 
-
+//teste
   constructor() { }
 
   ngOnInit(): void {
